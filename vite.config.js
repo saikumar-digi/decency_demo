f12ebf181a97ee9d8a4d44d21944a48994658d1a
saikumar <115986692+saikumar-digi@users.noreply.github.com>
2024-05-13 18:04:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const port = process.env.PORT || 4000; 
+const port = process.env.PORT || 10000; // Default to port 10000 if PORT environment variable is not set
 
 // https://vitejs.dev/config/
 export default defineConfig({
