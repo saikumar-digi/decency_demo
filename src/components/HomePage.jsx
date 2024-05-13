eb939d0298cc1db26074a26fa59cd6ee44647c88
saikumar <saikumar@digisprint.com>
2024-05-13 16:14:37 +0530

    commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <div className="max-w-4xl mx-auto p-8">
+      <div className="max-w-4xl mx-auto p-4">
         <img src="/src/assets/banner.jpg" alt="Home" className="w-full h-auto" />
       </div>
     </div>
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <nav className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 p-4 shadow-md h-20">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="text-black font-semibold text-xl" style={{ fontFamily: 'Arial, sans-serif' }}>Decency Construction</div>
+      <div style={{width:"110px",marginTop:"-6px",marginLeft:"-10px",height:"100px"}}>
+        <img  alt='Decency Construction' src='/src/assets/logo.jpg'/>
+        </div>
         <ul className="flex space-x-4 items-center m-2">
           <li>
             <a href="/" className="text-black hover:text-gray-200 font-bold text-lg" style={{ fontFamily: 'Arial, sans-serif' }}>Home</a>
